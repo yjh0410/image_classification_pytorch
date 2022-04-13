a7283f7c218a651bd086436c927d00670ed660a9
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-13 14:44:23 +0800

    train zitai dataset

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 python demo.py --cuda \
-                --data_path data/zitai/ \
+                --data_path data/zitai01/ \
                 --num_classes 2 \
                 -m resnet18 \
                 --weight weights/best_model.pth \
