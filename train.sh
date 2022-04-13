ff8336ae46e75041f026f5295ca29ef89b2ed8a1
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-13 14:39:57 +0800

    train zitai dataset

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 python train.py --cuda \
-                --data_path data/zitai/ \
+                --data_path data/zitai01/ \
                 --num_classes 2 \
                 -m resnet18 \
                 -p \
