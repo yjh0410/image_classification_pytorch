70a9b35b53f1c52aadc47d8512153fe9c7616b61
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-18 14:34:42 +0800

    updatye

--- train.sh
+++ train.sh
@@ -2,6 +2,7 @@ python train.py --cuda \
                 --data_path /mnt/share/ssd2/dataset/imagenet/ \
                 --num_classes 1000 \
                 -m cspd-s \
+                --wp_epoch 1 \
                 --max_epoch 300 \
                 --batch_size 128 \
                 --optimizer adamw \
