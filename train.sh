fdb1a2ac09e01340bff5e33a1a6ad0cc6c6bd270
yjh0410 <1394571815@qq.com>
2022-10-26 19:22:52 +0800

    update

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python train.py --cuda \
                 --data_path /mnt/share/ssd2/dataset/imagenet/ \
                 --num_classes 1000 \
-                -m elannet_huge \
+                -m elannet_nano \
                 --wp_epoch 20 \
-                --max_epoch 300 \
+                --max_epoch 200 \
                 --eval_epoch 10 \
                 --batch_size 128 \
                 --optimizer adamw \
