725dfe721eb7143a7473b271fea755c7790efc12
yjh0410 <1394571815@qq.com>
2022-11-07 13:39:20 +0800

    add ddp

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,14 +1,14 @@
 # 32 GPUs
 # Attention, the following batch size is on single GPU, not all GPUs.
-python  -m torch.distributed.run --nproc_per_node=2 train.py --cuda \
+python  -m torch.distributed.run --nproc_per_node=8 train.py --cuda \
                                                             -dist \
                                                             --data_path /mnt/share/ssd2/dataset/imagenet/ \
                                                             --num_classes 1000 \
-                                                            -m elannet_nano \
+                                                            -m elannet_huge \
                                                             --wp_epoch 20 \
                                                             --max_epoch 300 \
                                                             --eval_epoch 10 \
-                                                            --batch_size 1 \
+                                                            --batch_size 128 \
                                                             --optimizer adamw \
                                                             --base_lr 4e-3 \
                                                             --min_lr 1e-6 \
