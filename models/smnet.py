1891164592451bdc67aa9acc0d15d3894ef6d871
yjh0410 <1394571815@qq.com>
2023-07-17 09:13:43 +0800

    update

--- models/smnet.py
+++ models/smnet.py
@@ -2,7 +2,21 @@ import torch
 import torch.nn as nn
 
 
-# -------------------------- Basic Conv Module --------------------------
+# ---------------------------- 2D CNN ----------------------------
+class SiLU(nn.Module):
+    """export-friendly version of nn.SiLU()"""
+
+    @staticmethod
+    def forward(x):
+        return x * torch.sigmoid(x)
+
+
+def get_conv2d(c1, c2, k, p, s, d, g, bias=False):
+    conv = nn.Conv2d(c1, c2, k, stride=s, padding=p, dilation=d, groups=g, bias=bias)
+
+    return conv
+
+
 def get_activation(act_type=None):
     if act_type == 'relu':
         return nn.ReLU(inplace=True)
@@ -12,6 +26,8 @@ def get_activation(act_type=None):
         return nn.Mish(inplace=True)
     elif act_type == 'silu':
         return nn.SiLU(inplace=True)
+    elif act_type is None:
+        return nn.Identity()
 
 
 def get_norm(norm_type, dim):
@@ -29,28 +45,32 @@ class Conv(nn.Module):
                  p=0,                  # padding
                  s=1,                  # padding
                  d=1,                  # dilation
-                 act_type='silu',      # activation
+                 act_type='lrelu',     # activation
                  norm_type='BN',       # normalization
                  depthwise=False):
         super(Conv, self).__init__()
         convs = []
+        add_bias = False if norm_type else True
+        p = p if d == 1 else d
         if depthwise:
+            convs.append(get_conv2d(c1, c1, k=k, p=p, s=s, d=d, g=c1, bias=add_bias))
             # depthwise conv
-            convs.append(nn.Conv2d(c1, c1, kernel_size=k, stride=s, padding=p, dilation=d, groups=c1, bias=False))
-            convs.append(get_norm(norm_type, c1))
-            if act_type is not None:
+            if norm_type:
+                convs.append(get_norm(norm_type, c1))
+            if act_type:
                 convs.append(get_activation(act_type))
-
             # pointwise conv
-            convs.append(nn.Conv2d(c1, c2, kernel_size=1, stride=1, padding=0, dilation=d, groups=1, bias=False))
-            convs.append(get_norm(norm_type, c2))
-            if act_type is not None:
+            convs.append(get_conv2d(c1, c2, k=1, p=0, s=1, d=d, g=1, bias=add_bias))
+            if norm_type:
+                convs.append(get_norm(norm_type, c2))
+            if act_type:
                 convs.append(get_activation(act_type))
 
         else:
-            convs.append(nn.Conv2d(c1, c2, kernel_size=k, stride=s, padding=p, dilation=d, groups=1, bias=False))
-            convs.append(get_norm(norm_type, c2))
-            if act_type is not None:
+            convs.append(get_conv2d(c1, c2, k=k, p=p, s=s, d=d, g=1, bias=add_bias))
+            if norm_type:
+                convs.append(get_norm(norm_type, c2))
+            if act_type:
                 convs.append(get_activation(act_type))
             
         self.convs = nn.Sequential(*convs)
@@ -74,8 +94,8 @@ class MultiHeadMixedConv(nn.Module):
         # -------------- Network parameters --------------
         ## Scale Modulation
         self.mixed_convs = nn.ModuleList([
-            Conv(self.head_dim, self.head_dim, k=2*i+1, p=i, act_type=None, norm_type=norm_type, depthwise=depthwise)
-            for i in range(num_heads)])
+            Conv(self.head_dim, self.head_dim, k=2*i+1, p=i, act_type=None, norm_type=None, depthwise=depthwise)
+            for i in range(1, num_heads+1)])
         ## Aggregation proj
         self.out_proj = Conv(self.head_dim*num_heads, out_dim, k=1, act_type=act_type, norm_type=norm_type)
 
