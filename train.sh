7cf65e8096e4fca14ff76296720b5c6dae79e013
yjh0410 <1394571815@qq.com>
2024-03-02 22:23:02 +0800

    use standard pipeline

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ MASTER_PORT=$5
 RESUME=$6
 
 # ------------------- Training setting -------------------
-MAX_EPOCH=120
+MAX_EPOCH=90
 WP_EPOCH=-1
 EVAL_EPOCH=5
 BASE_LR=0.1
@@ -25,6 +25,7 @@ if [ $WORLD_SIZE == 1 ]; then
                     --batch_size ${BATCH_SIZE} \
                     --base_lr ${BASE_LR} \
                     --min_lr ${MIN_LR} \
+                    --use_pixel_statistic \
                     --resume ${RESUME}
 elif [[ $WORLD_SIZE -gt 1 && $WORLD_SIZE -le 8 ]]; then
     python -m torch.distributed.run --nproc_per_node=${WORLD_SIZE} --master_port ${MASTER_PORT} train.py \
@@ -38,8 +39,10 @@ elif [[ $WORLD_SIZE -gt 1 && $WORLD_SIZE -le 8 ]]; then
                     --batch_size ${BATCH_SIZE} \
                     --base_lr ${BASE_LR} \
                     --min_lr ${MIN_LR} \
+                    --use_pixel_statistic \
                     --world_size ${WORLD_SIZE} \
-                    --resume ${RESUME}
+                    --resume ${RESUME} \
+                    --sybn
 else
     echo "The WORLD_SIZE is set to a value greater than 8, indicating the use of multi-machine \
           multi-card training mode, which is currently unsupported."
